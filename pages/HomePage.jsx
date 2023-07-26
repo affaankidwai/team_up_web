c4338651e275de2c4292c1d4116aaf7a3ccdd5e0
Affaan Kidwai <kidwaiaffaan@gmail.com>
2023-07-26 23:35:06 +0530

    starting update

---
+++ pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <div>
+        <h1>Hi</h1>
+      </div>
+    </div>
+  );
+}
